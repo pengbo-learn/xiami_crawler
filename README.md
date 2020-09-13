e7ef01680ca364582184dbb9684d8063c5c8c07d
pengbo <1501210081@pku.edu.cn>
2020-09-13 09:14:24 +0800

    firefox

--- README.md
+++ README.md
@@ -26,12 +26,7 @@
 - python3
 - 配置
     sh env.sh
-    ```bash
-    #!/bin/bash
-    pip install tqdm==4.48.2
-    pip install selenium==3.141.0
-    cd rpms && yum install parallel-20150522-1.el7.cern.noarch.rpm
-    ```
+
 
 ## 运行
 ```bash
--- env.sh
+++ env.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 pip install tqdm==4.48.2
 pip install selenium==3.141.0
+wget https://ftp.mozilla.org/pub/firefox/releases/56.0.2/linux-x86_64/en-US/firefox-56.0.2.tar.bz2 -O driver/firefox.tar.bz2
+cd driver && tar jxvf firefox.tar.bz2 && cd ..
 cd rpms && yum install parallel-20150522-1.el7.cern.noarch.rpm
